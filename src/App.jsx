76fc19269d54117f07e139cc4610464cbc1df282
Ron Cruz <Ronolibert@thecoursekey.com>
2019-04-05 20:14:14 -0700

    Add foundational frontend structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import BasicExample from './BasicExample';
+
+function App() {
+  return (
+    <>
+      Hey
+      <BasicExample />
+    </>
+  );
+}
+
+export default App;
